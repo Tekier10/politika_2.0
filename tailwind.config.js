cf8306fc95797d92ae8615aea3cc590ded781d6b
Tekier10 <112965687+Tekier10@users.noreply.github.com>
2025-07-12 14:57:15 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}" // pro jistotu i novější Next.js struktury
   ],
   theme: {
     extend: {},
