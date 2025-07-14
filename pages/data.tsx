991b5ff1f271f085ed601ec06737d31ceb532a6c
Tekier10 <112965687+Tekier10@users.noreply.github.com>
2025-07-14 11:27:31 +0200

    Update data.tsx

--- pages/data.tsx
+++ pages/data.tsx
@@ -82,7 +82,7 @@ export default function DataPage() {
             <ResponsiveContainer width="100%" height={400}>
               <PieChart>
                 <Tooltip />
-                <Legend verticalAlign="right" layout="vertical" />
+                <Legend align="right" verticalAlign="middle" layout="vertical" />
                 <Pie
                   data={[
                     { name: 'Školství', value: 24 },
